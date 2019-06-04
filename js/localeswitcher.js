0f6109620f3147fa8006e5f978ea8a610b29d6fd
0dp <odinp123@gmail.com>
2019-06-04 11:04:14 +0200

    animation

--- js/localeswitcher.js
+++ js/localeswitcher.js
@@ -1,9 +1,47 @@
-(function() {
-    document.querySelector('.localeswitcher_toggle').addEventListener('click', function() {
-        document.querySelector('.localeswitcher_dropdown').classList.toggle('opening')
- 		 window.setTimeout(() => {
- 		 document.querySelector('.opening').classList.toggle('opened')
- 		 }, 333)
+// (function() {
+// console.log('ready');
+//     document.querySelector('.localeswitcher_toggle').addEventListener('click', function() {
+//     console.log('clicked');
+//         document.querySelector('.localeswitcher_dropdown').classList.toggle('opening')
+//  		 window.setTimeout(() => {
+//  		 document.querySelector('.opening').classList.toggle('opened')
+//  		 }, 333)
     	
-    }, false)
-})();
+//     }, false)
+// })();
+
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+
+var clicked = false;
+
+	$('.localeswitcher_toggle').on('click', function(e) {
+  	
+    	
+    	if(clicked) {
+            clicked = false;
+        	$('.localeswitcher_dropdown').addClass("opening")
+
+         	window.setTimeout(() => {
+        		$('.localeswitcher_dropdown').addClass('opened')
+         	}, 333)  
+        }
+        else {
+            clicked = true;
+        	$('.localeswitcher_dropdown').removeClass("opened")
+
+         	window.setTimeout(() => {
+        		$('.localeswitcher_dropdown').removeClass('opening')
+         	}, 1) 
+        }
+    
+    // .queue(function(){      
+    //   $('.localeswitcher_dropdown').
+    //   toggleClass('opened')
+    //   .dequeue();
+    // });
+       
+      return false;
+    });
+});
